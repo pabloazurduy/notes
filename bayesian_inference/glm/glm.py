0b1e969ae41939a1fc326c1288c86101fb4fd16e
pabloazurduy <pablo.azurduy@ug.uchile.cl>
2023-08-29 14:25:42 +0200

    newfeature: added functional posterior predictive

--- bayesian_inference/glm/glm.py
+++ bayesian_inference/glm/glm.py
@@ -4,6 +4,7 @@ from scipy import stats
 from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
 import arviz as az
+import xarray as xr
 
 
 np.random.seed(0)
@@ -11,44 +12,48 @@ n_samples = 1000
 education_years = np.random.normal(10, 2, (n_samples,2))
 b1_real = np.array([-1,5])
 b0_real = np.array([5_000, 5])
-income =  b0_real  + b1_real*education_years + stats.halfnorm.rvs(0, 20,(n_samples,2))
+y =  b0_real  + b1_real*education_years + stats.halfnorm.rvs(0, 20,(n_samples,2)) #income 
 
 
 with pm.Model() as model_city:
-    b0 = pm.Normal("b0", mu=0, sigma=10, shape=2)
+    b0 = pm.Normal("b0", mu=0, sigma=10_000, shape=2)
     b1 = pm.Normal("b1", mu=0, sigma=10, shape=2)
     sigma = pm.HalfNormal("sigma", sigma=10, shape=2)
     
-    mu = b0 + b1 * education_years
-    inc_mu = pm.Normal("inc_mu", mu=mu, sigma=sigma, observed=income, shape=income.shape)
+    mu = pm.Deterministic('mu', b0 + b1 * education_years)
+    inc_mu = pm.Normal("inc_mu", mu=mu, sigma=sigma, observed=y, shape=y.shape)
 
-    trace = pm.sample(draws=500, cores=4, target_accept=0.80)
-    posterior_predictive = pm.sample_posterior_predictive(trace, var_names=['b0', 'b1', 'inc_mu', 'sigma'])
+    trace = pm.sample(draws=500, cores=4, tune=400, return_inferencedata=True)
+    post_idata = pm.sample_posterior_predictive(trace) # extend_inferencedata=True predictions=True, return_inferencedata=True
     map = pm.find_MAP()
 
-az.plot_forest(trace, combined=True, hdi_prob=0.95)
+az.plot_forest(trace, combined=True, hdi_prob=0.95, var_names=['sigma','b1','b0'])
+posterior_pred = az.extract(post_idata, group='posterior_predictive', num_samples=100) 
+posterior      = az.extract(trace, group='posterior', num_samples=100) 
+y_pred = posterior_pred['inc_mu']
+y_trend = posterior['mu']
 
 # plot and add linear trend 
 fig, axs = plt.subplots(1, 2, figsize=(10, 5))
 for i, ax in enumerate(axs):
-    ax.scatter(education_years[:, i], income[:, i], s=2)
+    ax.scatter(education_years[:, i], y[:, i], s=2)
     ax.set_xlabel('Education Years')
-    ax.set_ylabel('Income')
+    ax.set_ylabel('income')
     ax.set_title (f'City {i}')
 
     # add linear regression trend line
     x = education_years[:, i]
-    y = income[:, i]
-    b1, b0, r_value, p_value, std_err = stats.linregress(x, y)
+    b1, b0, r_value, p_value, std_err = stats.linregress(x, y[:, i])
     ax.plot(x, b0 + b1*x, 'r', label=f'fitted line {b0=:.2} {b1=:.2}')
+    ax.plot(x, map['b0'][i] + map['b1'][i]*x, label=f'map_value {map["b0"][i]=:.2} {map["b1"][i]=:.2}', color='green')
     ax.legend()
-    y_pred = posterior_predictive['posterior_predictive']['inc_mu'].mean(axis=(0)).to_numpy()
-    ax.scatter(x=x,y=y_pred.mean(axis=0)[:,i], color='lightblue', label='prediction_mean', s=1) 
-    p05 = np.percentile(y_pred[:, :, i], 5, axis=0)
-    p95 = np.percentile(y_pred[:, :, i], 95, axis=0)
+    
+    ax.scatter(x=x,y=y_trend.mean(axis=2)[:,i], color='lightblue', label='prediction_mean', s=1) 
+    p05 = np.percentile(y_pred[:, i, :], 5, axis=1)
+    p95 = np.percentile(y_pred[:, i, :], 95, axis=1)
     f_p05 = interp1d(x, p05, kind='linear')
     f_p95 = interp1d(x, p95, kind='linear')
     xnew = np.linspace(min(x), max(x), 500)
     ax.fill_between(xnew, f_p05(xnew), f_p95(xnew), alpha=0.2, color='gray', interpolate=True)
-
 plt.show()
+
--- bayesian_inference/glm/linear_regression.md
+++ bayesian_inference/glm/linear_regression.md
@@ -3,15 +3,17 @@
 *created on: 2023-08-21 14:43:24*
 ### Linear Regression based on [BMCP][1]
 
+the major problem with the GLM NUTS sample when we have a non normalized dataset its on the GLM.py file, using [the documentation][3] lets see if we can improve the errors 
 
-
-
+To read https://www.pymc.io/projects/docs/en/latest/learn/core_notebooks/posterior_predictive.html
 
 
 
 
 [//]: <> (References)
 [1]: <https://bayesiancomputationbook.com/markdown/chp_02.html#divm0>
+[2]: <https://www.pymc.io/projects/docs/en/stable/learn/core_notebooks/GLM_linear.html>
+[3]: <https://www.pymc.io/projects/docs/en/stable/learn/core_notebooks/pymc_overview.html#case-study-1-educational-outcomes-for-hearing-impaired-children>
 
 [//]: <> (Some snippets)
 [//]: # (add an image <img src="" style='height:400px;'>)
